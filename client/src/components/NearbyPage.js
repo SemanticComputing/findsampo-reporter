b53659af4576623a12551018f2b121f5f9a72014
Hassanzadeh Pejam <pejam.hassanzadeh@aalto.fi>
2019-04-09 17:22:26 +0300

    Fix style of nearby page bar

--- client/src/components/NearbyPage.js
+++ client/src/components/NearbyPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Icon, FormControlLabel, Switch, CircularProgress } from '@material-ui/core';
+import { Icon, FormControlLabel, Switch, CircularProgress, Paper } from '@material-ui/core';
 import Map from './map/Map';
 import { getValidatedFinds } from '../actions/find';
 
@@ -13,25 +13,27 @@ class NearbyPage extends Component {
     return (
       <div className="nearby">
         <div className="nearby__tool-bar">
-          <Icon className="nearby__tool-bar__icon" >tune</Icon>
-          <FormControlLabel
-            className="nearby__tool-bar__form-control-label"
-            labelPlacement="start"
-            control={
-              <Switch
-                checked={true} // FIXME
-                color="primary"
-              />
-            }
-            label="Show Map"
-          />
+          <Paper className="nearby__tool-bar__paper"elevation={1}>
+            <Icon className="nearby__tool-bar__icon" >tune</Icon>
+            <FormControlLabel
+              className="nearby__tool-bar__form-control-label"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={true} // FIXME
+                  color="primary"
+                />
+              }
+              label="Show Map"
+            />
+          </Paper>
         </div>
         <div className="nearby__map">
           {
             this.props.finds ? (
               < Map markerData={this.props.finds} />
             ) : (
-              <CircularProgress size="5rem" />
+              <CircularProgress className="nearby__map__progress" size="5rem" />
             )
           }
         </div>
