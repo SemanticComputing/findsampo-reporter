be753116c4c80653afb4ac19350aa70765ace13a
Hassanzadeh Pejam <pejam.hassanzadeh@aalto.fi>
2019-08-08 10:12:41 +0300

    Change report current step when continuing filling out a report

--- client/src/reducers/report.js
+++ client/src/reducers/report.js
@@ -1,5 +1,9 @@
 import initalState from '../helpers/data/report';
-import { REPORT_CHANGE_QUESTION, FIND_NOTIFICATION_RESET } from '../constants/actionTypes';
+import {
+  REPORT_CHANGE_QUESTION,
+  FIND_NOTIFICATION_RESET,
+  MY_FINDS_CONTINUE_FILLING_OUT
+} from '../constants/actionTypes';
 
 const DEFAULT_CURRENT_STEP = 0;
 
@@ -14,6 +18,11 @@ export default (state = initalState, action) => {
       return {
         ...state,
         currentStep: DEFAULT_CURRENT_STEP
+      };
+    case MY_FINDS_CONTINUE_FILLING_OUT:
+      return {
+        ...state,
+        currentStep: action.report.currentStep
       };
     default:
       return state;
