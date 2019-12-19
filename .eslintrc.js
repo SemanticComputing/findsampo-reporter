174a39636c1c0d5a0124b1d1f213306c221b549d
Pejam Hassanzadeh <pejam.hassanzadeh@aalto.fi>
2019-12-19 10:16:55 +0200

    Add eslint settings for react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,54 @@
 module.exports = {
-    'rules': {
-      'indent': [
-        'error',
-        2,
-        { 'SwitchCase': 1 }
-      ],
-      'quotes': [
-        2,
-        'single',
-        { 'allowTemplateLiterals': true }
-      ],
-      'linebreak-style': [
-        2,
-        'unix'
-      ],
-      'semi': [
-        2,
-        'always'
-      ],
-      'no-console': 'off',
-      'react/jsx-uses-vars': ['error'],
-      'space-infix-ops': ['error', {'int32Hint': true}],
-      'react/prop-types': 0,
-      'react/display-name': 'off'
-    },
-    'env': {
-      'es6': true,
-      'node': true,
-      'browser': true
-    },
-    'extends': [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:import/errors',
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
     ],
-    'parser': 'babel-eslint',
-    'parserOptions': {
-      'ecmaVersion': 6,
-      'ecmaFeatures': {
-        'jsx': true,
-        'experimentalObjectRestSpread': true
-      },
-      'sourceType': 'module'
+    'quotes': [
+      2,
+      'single',
+      { 'allowTemplateLiterals': true }
+    ],
+    'linebreak-style': [
+      2,
+      'unix'
+    ],
+    'semi': [
+      2,
+      'always'
+    ],
+    'no-console': 'off',
+    'react/jsx-uses-vars': ['error'],
+    'space-infix-ops': ['error', { 'int32Hint': true }],
+    'react/prop-types': 0,
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  'env': {
+    'es6': true,
+    'node': true,
+    'browser': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'ecmaFeatures': {
+      'jsx': true,
+      'experimentalObjectRestSpread': true
     },
-    "settings": {
-      "react": {
-        "version": "detect", 
-      }
-    },
-    'plugins': ['react']
-  };
+    'sourceType': 'module'
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    }
+  },
+  'plugins': ['react', 'react-hooks']
+};
