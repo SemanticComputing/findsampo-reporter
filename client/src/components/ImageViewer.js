122f2a1f478dd87b0a46ca0c8192401915d713d7
Pejam Hassanzadeh <pejam.hassanzadeh@aalto.fi>
2019-12-31 14:13:40 +0200

    Use thumb image for preview in image viewer

--- client/src/components/ImageViewer.js
+++ client/src/components/ImageViewer.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Lightbox from 'react-image-lightbox';
+import { getThumbId } from '../utils/functions/functions';
+
 import 'react-image-lightbox/style.css';
+
 
 const ImageViewer = (props) => {
   const [isOpen, setLighboxStatus] = useState(false);
@@ -8,7 +11,7 @@ const ImageViewer = (props) => {
   return (
     <div style={{ display: props.display || 'inline', margin: '2px', cursor: 'pointer' }}>
       <img
-        src={props.image}
+        src={getThumbId(props.image)}
         width={props.width ? `${props.width}px` : '64px'}
         height={props.height ? `${props.height}px` : '64px'}
         onClick={() => setLighboxStatus(true)}
